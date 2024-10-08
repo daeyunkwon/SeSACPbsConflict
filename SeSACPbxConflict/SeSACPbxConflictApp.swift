20e099c2c1a2a871c731a45d169d2e3f17e136a7
Daeyun <sweet_ray@naver.com>
2024-10-08 11:25:07 +0900

    프로젝트 init

---
+++ SeSACPbxConflict/SeSACPbxConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACPbxConflictApp.swift
+//  SeSACPbxConflict
+//
+//  Created by 권대윤 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SeSACPbxConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
